ea2f807d0676724e38cf676ed99c1c6897f12733
Laura de Nooij <Lauradenooij@hotmail.com>
2021-09-15 11:16:41 +0200

    update lab member functions

--- build.py
+++ build.py
@@ -57,7 +57,7 @@ def create_html_files():
     members_exttr = [d for d in members if d['function'] == 'External Trainee']
     members_exttr.sort(key=lambda x: x["name"].split(" ")[-1])
     members_former = [d for d in members if d['function'] == 'Former Lab Members']
-    members = members_pi + members_sr + members_postdoc + members_phd + members_extphd + members_ra + members_tr + members_former
+    members = members_pi + members_sr + members_postdoc + members_phd + members_extphd + members_visitphd + members_ra + members_tr + members_exttr + members_former
 
     context = {
             'members': members
